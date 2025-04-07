80d4245fb183b7ac7e2730778bdb592f65f273d2
farnaz <farnaz.towhidi@gmail.com>
2025-04-07 18:11:12 -0400

    update

---
+++ JavaScript/JavaScript1_Object.js
@@ -0,0 +1,18 @@
+students = [
+  {
+  name: "Kameron Handy",
+  age: 42,
+  isEnrolled: false
+}, 
+{
+  name: "Mebin Beharry",
+  age: 32,
+  isEnrolled: true
+}, {
+  name: "Tanner day",
+  age: 24,
+  isEnrolled: true
+}
+]
+
+console.log (students[0].name)

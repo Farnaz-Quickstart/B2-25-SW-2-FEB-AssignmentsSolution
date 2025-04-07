1871a8f87291c72c0d3242cf67a49c6d4858ead4
farnaz <farnaz.towhidi@gmail.com>
2025-04-07 19:18:16 -0400

    Update assignment javascript, objects

---
+++ JavaScript/Script/Assignment1_Objects.js
@@ -0,0 +1,30 @@
+let colors = ["blue", "red", "black"] 
+
+let person = {
+  name: "Farnaz Towhidi",
+  age: 49,
+  bootcamp: "Software Engineering"
+}
+
+let students = [
+  { name: "Erica Valentin",
+    age: 32,
+    isEnrolled: true
+  }, 
+  { name: "Mebin Behhary",
+    age: 42,
+    isEnrolled: false
+  },
+  { name: "Tanner Day",
+    age: 25,
+    isEnrolled: true
+  }
+]
+
+console.log (students)
+console.log (students[0])
+console.log (students[0].age)
+console.log (students[2].name)
+console.log (typeof (students[2].name))
+console.log (typeof (students[2].age))
+console.log (typeof (students[2].isEnrolled))

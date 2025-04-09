0ff66150c0ef245303fbedd0825df075f3f761bb
farnaz <farnaz.towhidi@gmail.com>
2025-04-09 19:32:05 -0400

    update

---
+++ JavaScript/Script/Assignment2_function.js
@@ -0,0 +1,23 @@
+function studentGrading (studentName, studentScore) {
+  // 90 - 100.           A
+  // 80 - 89.            B
+  // 70 - 79             C
+
+  let studentGrade 
+
+  if ((studentScore>=90) && (studentScore <=100)) 
+    studentGrade = "A"
+  else if ((studentScore >=80) && (studentScore < 90)) 
+    studentGrade = "B"
+  else if (studentScore >=70 && studentScore < 80) 
+    studentGrade = "C"
+  else 
+    studentGrade = "F"
+  
+  console.log (`Hello ${studentName}, your score is ${studentScore} and your grade is ${studentGrade}`)
+
+}
+
+studentGrading("Kameron", 96)
+studentGrading("Farnaz", 82)
+studentGrading("Tom", 72)
